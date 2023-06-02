018ad4b249a2d980e039a082ec2ff4d177ccc8fa
mrossigrad <mrossi.grad@gmail.com>
2023-06-02 11:08:36 -0400

    shuffle false

--- trainer_conditioned.py
+++ trainer_conditioned.py
@@ -86,9 +86,9 @@ class Trainer():
             n_steps=self.n_steps,
             device=self.device,
         )
-        # Create dataloader
+        # Create dataloader (shuffle False for validation)
         self.data_loader_train = DataLoader(dataset=Data(path=self.dataset, mode="train", size=(self.image_size,self.image_size)), batch_size=self.batch_size, num_workers=0, drop_last=True, shuffle=True, pin_memory=True)
-        self.data_loader_val = DataLoader(dataset=Data(path=self.dataset, mode="val", size=(self.image_size,self.image_size)), batch_size=self.n_samples, num_workers=0, drop_last=True, shuffle=True, pin_memory=True)
+        self.data_loader_val = DataLoader(dataset=Data(path=self.dataset, mode="val", size=(self.image_size,self.image_size)), batch_size=self.n_samples, num_workers=0, drop_last=True, shuffle=False, pin_memory=True)
 
         # Create optimizer
         self.optimizer = torch.optim.Adam(self.eps_model.parameters(), lr=self.learning_rate)
