9d0112a8a332c64b76e0d71aea6960a0c484e5f9
mrossigrad <mrossi.grad@gmail.com>
2023-06-07 19:47:09 -0400

    new dataset

--- process_gopro.py
+++ process_gopro.py
@@ -6,7 +6,7 @@ i = 1
 for dir in gopro_dirs:
 
     sharp_imgs = os.listdir(dir+"/sharp")
-    blur_imgs = os.listdir(dir+"/train/blur")
+    blur_imgs = os.listdir(dir+"/blur")
 
     for s_img in sharp_imgs:
         os.rename(dir+"/sharp/"+s_img, dir+"/sharp/"+i+s_img)
