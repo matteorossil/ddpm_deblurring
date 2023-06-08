bb871e549ac6d826ac585efccb166b6c94c83f65
mrossigrad <mrossi.grad@gmail.com>
2023-06-08 13:15:48 -0400

    sys for epoch

--- trainer_unconditioned_sampling.py
+++ trainer_unconditioned_sampling.py
@@ -41,9 +41,9 @@ class Trainer():
     # Number of time steps $T$
     n_steps: int = 2_000
     # Number of sample images
-    n_samples: int = 4
+    n_samples: int = int(sys.argv[1])
     # checkpoint path
-    epoch = int(sys.argv[1])
+    epoch = int(sys.argv[2])
     checkpoint = f'/home/mr6744/checkpoints_distributed/06082023_001509/checkpoint_{epoch}.pt'
     # store sample
     sampling_path = '/home/mr6744/checkpoints_distributed/06082023_001509/sampling/'
