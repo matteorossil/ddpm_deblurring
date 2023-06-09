1083407db20993919268e1a343292cfcd141a6e8
mrossigrad <mrossi.grad@gmail.com>
2023-06-09 15:55:35 -0400

    sampling

--- trainer_unconditioned_sampling.py
+++ trainer_unconditioned_sampling.py
@@ -102,15 +102,15 @@ class Trainer():
                 t_vec = x.new_full((self.n_samples,), t, dtype=torch.long)
                 x = self.diffusion.p_sample(x, t_vec)
 
-            # Normalize img
-            #min_val = x.min(-1)[0].min(-1)[0]
-            #max_val = x.max(-1)[0].max(-1)[0]
-            #x_norm = (x-min_val[:,:,None,None])/(max_val[:,:,None,None]-min_val[:,:,None,None])
+                # Normalize img
+                #min_val = x.min(-1)[0].min(-1)[0]
+                #max_val = x.max(-1)[0].max(-1)[0]
+                #x_norm = (x-min_val[:,:,None,None])/(max_val[:,:,None,None]-min_val[:,:,None,None])
 
-            # save sampled images
-            if ((t_+1) % 1000 == 0):
-                save_image(x, os.path.join(self.sampling_path, f"epoch{self.epoch}_t{t_+1}.png"))
-            #save_image(x_norm, os.path.join(self.sampling_path, f"epoch{self.epoch}_t{t_+1}_norm.png"))
+                # save sampled images
+                if ((t_+1) % 1000 == 0):
+                    save_image(x, os.path.join(self.sampling_path, f"epoch{self.epoch}_t{t_+1}.png"))
+                    #save_image(x_norm, os.path.join(self.sampling_path, f"epoch{self.epoch}_t{t_+1}_norm.png"))
 
             return x
 
