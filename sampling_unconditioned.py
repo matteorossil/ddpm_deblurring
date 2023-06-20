3082244698eee991246717a5d6f54b61e3074eab
mrossigrad <mrossi.grad@gmail.com>
2023-06-20 14:44:52 -0400

    sampling unconditional

--- sampling_unconditioned.py
+++ sampling_unconditioned.py
@@ -106,7 +106,7 @@ class Trainer():
             save_image(blur, os.path.join(self.sampling_path, f"blur.png"))
 
             for i in range(1, 11):
-                t = torch.tensor([self.n_steps // i], device=blur.device)
+                t = torch.tensor([self.n_steps // i], device=blur.device, dtype=torch.long)
                 t = t.repeat(blur.shape[0])
                 noise = torch.randn_like(blur)
                 blur_noise = self.diffusion.q_sample(blur, t, eps=noise)
