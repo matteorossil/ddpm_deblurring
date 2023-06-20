eb70986193bd5d1f7173b39096b26b73f2168a9d
mrossigrad <mrossi.grad@gmail.com>
2023-06-20 15:17:26 -0400

    sampling unconditional

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ from torch.autograd import Variable
 
 def gather(consts: torch.Tensor, t: torch.Tensor):
     """Gather consts for $t$ and reshape to feature map shape"""
+    print(consts)
     c = consts.gather(-1, t)
     return c.reshape(-1, 1, 1, 1)
 
