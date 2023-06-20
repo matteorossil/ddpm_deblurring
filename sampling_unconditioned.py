ebf47a5cee22c17ea3ccac7f9fc7b1c78c8e9161
mrossigrad <mrossi.grad@gmail.com>
2023-06-20 14:59:41 -0400

    sampling unconditional

--- sampling_unconditioned.py
+++ sampling_unconditioned.py
@@ -108,6 +108,7 @@ class Trainer():
             for i in range(1, 11):
                 t = torch.tensor([self.n_steps // i])
                 t = t.repeat(blur.shape[0])
+                print(t)
                 t = t.to("cuda")
                 noise = torch.randn_like(blur)
                 blur_noise = self.diffusion.q_sample(blur, t, eps=noise)
