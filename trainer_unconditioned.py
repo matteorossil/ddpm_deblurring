07e571f0cca36022323e3d0ce1ee8f68730e7295
mrossigrad <mrossi.grad@gmail.com>
2023-06-27 16:11:30 -0400

    improved setup

--- trainer_unconditioned.py
+++ trainer_unconditioned.py
@@ -3,6 +3,7 @@
 
 from typing import List
 import os
+import sys
 import torch
 import torch.utils.data
 from diffusion.ddpm_unconditioned import DenoiseDiffusion
@@ -12,23 +13,24 @@ from datetime import datetime
 import wandb
 import torch.nn.functional as F
 
-from dataset import Data
+from dataset_unconditioned import Data
 from torch.utils.data import DataLoader
 from torchvision.utils import save_image
 
-
+import torch.multiprocessing as mp
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
+ 
 def get_exp_path(path=''):
     exp_path = os.path.join(path, datetime.now().strftime("%m%d%Y_%H%M%S"))
     Path(exp_path).mkdir(parents=True, exist_ok=True)
     return exp_path
 
-
 class Trainer():
     """
     ## Configurations
     """
-    # Device to train the model on.
-    device: torch.device = 'cuda' # change to 'cuda'
     # Number of channels in the image. $3$ for RGB.
     image_channels: int = 3
     # Image size
@@ -39,60 +41,81 @@ class Trainer():
     # The number of channels is `channel_multipliers[i] * n_channels`
     channel_multipliers: List[int] = [1, 2, 3, 4]
     # The list of booleans that indicate whether to use attention at each resolution
-    is_attention: List[int] = [False, False, False, False]
+    is_attention: List[int] = [False, False, False, True]
+    attention_middle: List[int] = [True]
     # Number of time steps $T$
     n_steps: int = 2_000
+    # noise scheduler
+    beta_0 = 1e-6 # 0.000001
+    beta_T = 1e-2 # 0.01
     # Batch size
-    batch_size: int = 4
+    batch_size: int = 32
     # Learning rate
     learning_rate: float = 1e-4
     # Weight decay rate
     weight_decay_rate: float = 1e-3
     # ema decay
-    betas = (0.9999, 0.9999)
+    betas = (0.9, 0.999)
     # Number of training epochs
-    epochs: int = 1_000
+    epochs: int = 10_000
     # Number of sample images
-    n_samples: int = 4
+    n_samples: int = 8
     # Use wandb
-    wandb: bool = False
+    wandb: bool = True
     # where to store the checkpoints
-    store_checkpoints: str = '/home/mr6744/checkpoints/'
-    #store_checkpoints: str = '/Users/m.rossi/Desktop/research/'
+    store_checkpoints: str = '/scratch/mr6744/pytorch/checkpoints_unconditioned/'
+    #store_checkpoints: str = '/home/mr6744/checkpoints_unconditioned/'
     # where to training and validation data is stored
-    dataset = '/home/mr6744/gopro/'
-    #dataset = '/Users/m.rossi/Desktop/research/ddpm_deblurring/dataset/'
+    dataset: str = '/scratch/mr6744/pytorch/gopro/'
+    #dataset: str = '/home/mr6744/gopro/'
     # load from a checkpoint
-    checkpoint_epoch = 0
-    checkpoint = f'/home/mr6744//checkpoints/06012023_194937/checkpoint_{checkpoint_epoch}.pt'
+    checkpoint_epoch: int = 0
+    checkpoint: str = f'/scratch/mr6744/pytorch/checkpoints_unconditioned/06252023_230516/checkpoint_{checkpoint_epoch}.pt'
+    #checkpoint: str = f'/home/mr6744/checkpoints_distributed/06092023_132041/checkpoint_{checkpoint_epoch}.pt'
 
-    def init(self):
+    def init(self, rank: int):
+        # gpu id
+        self.gpu_id = rank
+
         # Create $\epsilon_\theta(x_t, t)$ model
         self.eps_model = UNet(
             image_channels=self.image_channels,
             n_channels=self.n_channels,
             ch_mults=self.channel_multipliers,
             is_attn=self.is_attention,
-        ).to(self.device)
+            attn_middle=self.attention_middle
+        )
+
+        self.eps_model = self.eps_model.to(self.gpu_id)
+        self.eps_model = DDP(self.eps_model, device_ids=[self.gpu_id])
 
         # only load checpoint if model is trained
         if self.checkpoint_epoch != 0:
+            #map_location = {'cuda:%d' % 0: 'cuda:%d' % self.gpu_id}
+            #checkpoint_ = torch.load(self.checkpoint, map_location=map_location)
             checkpoint_ = torch.load(self.checkpoint)
-            self.eps_model.load_state_dict(checkpoint_)
+            self.eps_model.module.load_state_dict(checkpoint_)
 
         # Create DDPM class
         self.diffusion = DenoiseDiffusion(
             eps_model=self.eps_model,
             n_steps=self.n_steps,
-            device=self.device,
+            device=self.gpu_id,
+            beta_0=self.beta_0,
+            beta_T=self.beta_T
         )
         # Create dataloader
-        self.data_loader = DataLoader(dataset=Data(path=self.dataset, mode="train", size=(self.image_size,self.image_size)), batch_size=self.batch_size, num_workers=2, drop_last=True, shuffle=True, pin_memory=True)
+        dataset = Data(path=self.dataset, mode="train", size=(self.image_size,self.image_size))
+        self.dataloader_train = DataLoader(dataset=dataset,
+                                    batch_size=self.batch_size,
+                                    num_workers=24,
+                                    drop_last=False,
+                                    shuffle=False,
+                                    pin_memory=False,
+                                    sampler=DistributedSampler(dataset)) # assures no overlapping samples
 
         # Create optimizer
-        #self.optimizer = torch.optim.Adam(self.eps_model.parameters(), lr=self.learning_rate)
-        params = list(self.eps_model.parameters())
-        self.optimizer = torch.optim.AdamW(params, lr=self.learning_rate, weight_decay= self.weight_decay_rate, betas=self.betas)
+        self.optimizer = torch.optim.AdamW(self.eps_model.parameters(), lr=self.learning_rate, weight_decay= self.weight_decay_rate, betas=self.betas)
         self.step = 0
         self.exp_path = get_exp_path(path=self.store_checkpoints)
 
@@ -103,21 +126,23 @@ class Trainer():
         with torch.no_grad():
             # $x_T \sim p(x_T) = \mathcal{N}(x_T; \mathbf{0}, \mathbf{I})$
             # Sample Initial Image (Random Gaussian Noise)
+            torch.cuda.manual_seed(0)
             x = torch.randn([n_samples, self.image_channels, self.image_size, self.image_size],
-                            device=self.device)
+                            device=self.gpu_id)
             # Remove noise for $T$ steps
+            #for t_ in range(self.n_steps):
             for t_ in range(self.n_steps):
                 # $t$
                 t = self.n_steps - t_ - 1
                 # Sample from $p_\theta(x_{t-1}|x_t)$
                 t_vec = x.new_full((n_samples,), t, dtype=torch.long)
                 x = self.diffusion.p_sample(x, t_vec)
-            # Log samples
-            if self.wandb:
-                wandb.log({'samples': wandb.Image(x)}, step=self.step)
+            
+            save_image(x, os.path.join(self.exp_path, f'epoch{epoch}_t{t_+1}.png'))
 
-            # save sampled images
-            save_image(x, os.path.join(self.exp_path, f'epoch_{epoch}.png'))
+            # Log samples
+            #if self.wandb:
+                #wandb.log({'samples': wandb.Image(x)}, step=self.step)
 
             return x
 
@@ -126,11 +151,11 @@ class Trainer():
         ### Train
         """
         # Iterate through the dataset
-        for batch_idx, (sharp, blur) in enumerate(self.data_loader):
+        for batch_idx, sharp in enumerate(self.dataloader_train):
             # Increment global step
             self.step += 1
             # Move data to device
-            sharp = sharp.to(self.device)
+            sharp = sharp.to(self.gpu_id)
             # Make the gradients zero
             self.optimizer.zero_grad()
             # Calculate loss
@@ -141,28 +166,64 @@ class Trainer():
             self.optimizer.step()
             # Track the loss
             #print('loss:', loss, 'step:', self.step)
-            wandb.log({'loss': loss}, step=self.step)
+            if self.wandb and self.gpu_id == 0:
+                wandb.log({'loss': loss}, step=self.step)
 
     def run(self):
         """
         ### Training loop
         """
         for epoch in range(self.epochs):
-            if epoch % 10 == 0:
-                # Sample some images
-                self.sample(self.n_samples, epoch)
+            if (epoch == 0) and (self.gpu_id == 0):
+                self.sample(self.n_samples, self.checkpoint_epoch+epoch)
+
             # Train the model
             self.train()
-            if (epoch+1) % 10 == 0:
+            if ((epoch+1) % 20 == 0) and (self.gpu_id == 0):
                 # Save the eps model
-                torch.save(self.eps_model.state_dict(), os.path.join(self.exp_path, f'checkpoint_{epoch+1}.pt'))
+                self.sample(self.n_samples, self.checkpoint_epoch+epoch+1)
+                torch.save(self.eps_model.module.state_dict(), os.path.join(self.exp_path, f'checkpoint_{self.checkpoint_epoch+epoch+1}.pt'))
 
+def ddp_setup(rank, world_size):
+    """
+    Args:
+        rank: unique identifier of each process
+        world_size: total number of processes
+    """ 
+    # IP address of machine running rank 0 process
+    # master: machine coordinates communication across processes
+    os.environ["MASTER_ADDR"] = "localhost" # we assume a single machine setup)
+    os.environ["MASTER_PORT"] = "12355" # any free port on machine
+    # nvidia collective comms library (comms across CUDA GPUs)
+    init_process_group(backend="nccl", rank=rank, world_size=world_size)
 
-def main():
-    wandb.init()
+def main(rank: int, world_size:int):
+    ddp_setup(rank=rank, world_size=world_size)
     trainer = Trainer()
-    trainer.init() # initialize trainer class
+    trainer.init(rank) # initialize trainer class
+    
+    if trainer.wandb and rank == 0:
+
+        params = sum(p.numel() for p in trainer.eps_model.parameters() if p.requires_grad)
+        
+        wandb.init(
+            project="deblurring",
+            name=f"unconditioned",
+            config=
+            {
+            "GPUs": world_size,
+            "GPU Type": torch.cuda.get_device_name(rank),
+            "denoiser # params": params,
+            "dataset": trainer.dataset,
+            "loaded from checkpoint": trainer.checkpoint,
+            "checkpoints saved at": trainer.exp_path
+            }
+        )
+
     trainer.run() # perform training
+    destroy_process_group()
 
 if __name__ == "__main__":
-    main()
+    #world_size = torch.cuda.device_count() # how many GPUs available in the machine
+    world_size = 2
+    mp.spawn(main, args=(world_size,), nprocs=world_size)
