2ef4aad785b89967075164856b59f92470ced6cc
mrossigrad <mrossi.grad@gmail.com>
2023-06-30 00:06:38 -0400

    train u

--- test_init_predictor.py
+++ test_init_predictor.py
@@ -35,14 +35,14 @@ class Trainer():
     # Define sampling
 
     # Number of sample images
-    n_samples: int = 64
+    n_samples: int = 512
     # checkpoint path
     epoch: int = 5600
     #checkpoint: str = f'/scratch/mr6744/pytorch/checkpoints_init_predictor/06182023_103900/checkpoint_{epoch}.pt'
     checkpoint = f'/home/mr6744/checkpoints_init_predictor/checkpoint_{epoch}.pt'
     # store sample
     #sampling_path: str = f'/scratch/mr6744/pytorch/checkpoints_init_predictor/sample/'
-    sampling_path = '/home/mr6744/checkpoints_init_predictor/sample/'
+    sampling_path = '/home/mr6744/checkpoints_init_predictor/sample2/'
     # dataset
     #dataset: str = '/scratch/mr6744/pytorch/gopro/'
     dataset: str = '/home/mr6744/gopro/'
