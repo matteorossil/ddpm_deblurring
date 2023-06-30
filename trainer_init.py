1fc3152224dac58dfeb8acbb709b107769947f35
mrossigrad <mrossi.grad@gmail.com>
2023-06-30 10:36:06 -0400

    train u

--- trainer_init.py
+++ trainer_init.py
@@ -62,8 +62,8 @@ class Trainer():
     dataset: str = '/scratch/mr6744/pytorch/gopro/'
     #dataset: str = '/Users/m.rossi/Desktop/research/ddpm_deblurring/dataset/'
     # load from a checkpoint
-    checkpoint_epoch: int = 5560
-    checkpoint: str = f'/scratch/mr6744/pytorch/checkpoints_init_predictor/06272023_192601/checkpoint_{checkpoint_epoch}.pt'
+    checkpoint_epoch: int = 5900
+    checkpoint: str = f'/scratch/mr6744/pytorch/checkpoints_init_predictor/06292023_221141/checkpoint_{checkpoint_epoch}.pt'
     #checkpoint: str = f'/home/mr6744/checkpoints_init_predictor/06152023_200330/checkpoint_{checkpoint_epoch}.pt'
 
     def init(self, rank: int):
@@ -91,7 +91,7 @@ class Trainer():
 
         self.data_loader_train = DataLoader(dataset=dataset_train,
                                             batch_size=self.batch_size, 
-                                            num_workers=16,
+                                            num_workers=20,
                                             #num_workers=os.cpu_count() // 4, 
                                             drop_last=True, 
                                             shuffle=False, 
