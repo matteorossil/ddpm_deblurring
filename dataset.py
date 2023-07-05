43dca1d1e5c8c76999a7839b5a8caa7e87b4ff3c
mrossigrad <mrossi.grad@gmail.com>
2023-07-05 11:30:13 -0400

    train C

--- dataset.py
+++ dataset.py
@@ -86,9 +86,9 @@ class Data(Dataset):
 
     def transform_train2(self, sharp, blur):
 
-        torch.manual_seed(0)
+        #torch.manual_seed(0)
         #torch.cuda.manual_seed_all(0)
-        random.seed(0)
+        #random.seed(0)
 
         # Random crop
         i, j, h, w = transforms.RandomCrop.get_params(sharp, output_size=self.size)
