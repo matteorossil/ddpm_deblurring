92e0c14b69bc8e1efc8eea83931583156b1bb873
mrossigrad <mrossi.grad@gmail.com>
2023-07-12 17:30:55 -0400

    cleaned code for conditioned

--- trainer_conditioned.py
+++ trainer_conditioned.py
@@ -316,8 +316,8 @@ class Trainer():
         #print(self.init_predictor.module.final.bias.grad)
 
         # clip gradients
-        nn.utils.clip_grad_norm_(self.params_denoiser, 1.0)
-        nn.utils.clip_grad_norm_(self.params_init, 1.0)
+        nn.utils.clip_grad_norm_(self.params_denoiser, 0.2)
+        nn.utils.clip_grad_norm_(self.params_init, 0.2)
 
         # Take an optimization step
         self.optimizer.step()
