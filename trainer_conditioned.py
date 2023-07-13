3b83e39d85495e17d8c27bd65fce2a91f8ed05aa
mrossigrad <mrossi.grad@gmail.com>
2023-07-13 11:19:58 -0400

    cleaned code for conditioned

--- trainer_conditioned.py
+++ trainer_conditioned.py
@@ -90,7 +90,7 @@ class Trainer():
     # noise scheduler Beta_T
     beta_T = 1e-2 # 0.01
     # Batch size
-    batch_size: int = 32
+    batch_size: int = 1
     # Learning rate
     learning_rate: float = 1e-4
     # Weight decay rate
@@ -100,7 +100,7 @@ class Trainer():
     # Number of training epochs
     epochs: int = 100_000
     # Number of samples (evaluation)
-    n_samples: int = 32
+    n_samples: int = 1
     # Use wandb
     wandb: bool = False
     # checkpoints path
