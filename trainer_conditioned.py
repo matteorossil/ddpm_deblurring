cff794cc954a7e1532db95f26f48ed4681c0259e
mrossigrad <mrossi.grad@gmail.com>
2023-07-14 15:53:16 -0400

    conditioned

--- trainer_conditioned.py
+++ trainer_conditioned.py
@@ -160,7 +160,7 @@ class Trainer():
         )
 
         # Create dataloader (shuffle False for validation)
-        dataset_train = Data(path=self.dataset, mode="train", size=(self.image_size,self.image_size))
+        dataset_train = Data(path=self.dataset, mode="val", size=(self.image_size,self.image_size))
         dataset_val = Data(path=self.dataset, mode="val", size=(self.image_size,self.image_size))
 
         self.dataloader_train = DataLoader(dataset=dataset_train,
