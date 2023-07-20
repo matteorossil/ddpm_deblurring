9abb505cab6c3b6d7cc0fab2d1b7725bc56a32d3
mrossigrad <mrossi.grad@gmail.com>
2023-07-20 11:14:38 -0400

    conditioned

--- trainer_conditioned.py
+++ trainer_conditioned.py
@@ -343,7 +343,7 @@ class Trainer():
         #else: alpha = 0. #0.01
 
         # denoiser loss
-        denoiser_loss = self.diffusion.loss(residual, blur) * 0.001
+        denoiser_loss = self.diffusion.loss(residual, blur)
 
         # initial predictor loss
         regression_loss = alpha * F.mse_loss(sharp, init)
