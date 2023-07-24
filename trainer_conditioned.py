43c9d2487688803b3fbb8e66dbff4480a3328e1a
mrossigrad <mrossi.grad@gmail.com>
2023-07-24 16:28:46 -0400

    conditioned

--- trainer_conditioned.py
+++ trainer_conditioned.py
@@ -422,7 +422,7 @@ class Trainer():
         for epoch in range(self.epochs):
 
             # sample at epoch 0
-            if (self.step == 0) and (self.gpu_id == 0):
+            if (epoch == 0) and (self.gpu_id == 0):
                 #pass 
                 self.sample("train", self.dataloader_train2, psnr_init_t, ssim_init_t, psnr_deblur_t, ssim_deblur_t)
                 self.sample("val", self.dataloader_val, psnr_init_v, ssim_init_v, psnr_deblur_v, ssim_deblur_v)
@@ -431,12 +431,11 @@ class Trainer():
             # train
             self.train(epoch+1, steps, R, G, B, ch_blur)
 
-            if (self.step % 100 == 0) and (self.gpu_id == 0):
-                print("enter", self.step)
+            if (epoch % 2 == 0) and (self.gpu_id == 0):
                 title = f"Init - D:{self.num_params_denoiser//1_000_000}M, G:{self.num_params_init//1_000_000}M, Pre:No, D:{'{:.0e}'.format(self.learning_rate)}, G:{'{:.0e}'.format(self.learning_rate_init)}, B:{self.batch_size}, RGB:{ch_blur}"
                 plot_channels(steps, R, G, B, self.exp_path, title=title, ext="init_")
 
-            if (self.step % 200 == 0) and (self.gpu_id == 0):
+            if (epoch % 2 == 0) and (self.gpu_id == 0):
                 self.sample("train", self.dataloader_train2, psnr_init_t, ssim_init_t, psnr_deblur_t, ssim_deblur_t)
                 self.sample("val", self.dataloader_val, psnr_init_v, ssim_init_v, psnr_deblur_v, ssim_deblur_v)
                 sample_steps.append(self.step)
