0dcc454ef96f9a70760d2dad68fbcff88cd2acc7
mrossigrad <mrossigrad@gmail.com>
2023-08-07 18:13:23 -0400

    hpc

--- trainer_conditioned_3.py
+++ trainer_conditioned_3.py
@@ -26,6 +26,7 @@ from pathlib import Path
 from datetime import datetime
 import wandb
 import matplotlib.pyplot as plt
+import argparse
 
 # DDP
 import torch.multiprocessing as mp
@@ -86,57 +87,59 @@ class Trainer():
     """
     ## Configurations
     """
-    # Number of channels in the image. $3$ for RGB.
-    image_channels: int = 3
-    # Image size
-    image_size: int = 128
-    # Number of channels in the initial feature map
-    n_channels: int = 32
-    # The list of channel numbers at each resolution.
-    # The number of channels is `channel_multipliers[i] * n_channels`
-    channel_multipliers: List[int] = [1, 2, 3, 4]
-    # The list of booleans that indicate whether to use attention at each resolution
-    is_attention: List[int] = [False, False, False, False]
-    # Number of time steps $T$
-    n_steps: int = 1_000
-    # noise scheduler Beta_0
-    beta_0 = 1e-6 # 0.000001
-    # noise scheduler Beta_T
-    beta_T = 1e-2 # 0.01
-    # Batch size
-    batch_size: int = 16
-    # L2 loss
-    alpha = 0.
-    # Threshold Regularizer
-    threshold = 0.02
-    # Learning rate
-    learning_rate: float = 1e-4
-    learning_rate_init: float = 1e-4
-    # Weight decay rate
-    weight_decay_rate: float = 1e-3
-    # ema decay
-    betas = (0.9, 0.999)
-    # Number of training epochs
-    epochs: int = 1_000_000
-    # Number of samples (evaluation)
-    n_samples: int = 16
-    # Use wandb
-    wandb: bool = True
-    # checkpoints path
-    #store_checkpoints: str = '/home/mr6744/ckpts/'
-    store_checkpoints: str = '/scratch/mr6744/pytorch/ckpts/'
-    # dataset path
-    #dataset_t: str = '/home/mr6744/gopro_small/'
-    dataset_t: str = '/scratch/mr6744/pytorch/gopro_small_multi/'
-    #dataset_v: str = '/home/mr6744/gopro_small/'
-    dataset_v: str = '/scratch/mr6744/pytorch/gopro_small_multi/'
-    # load from a checkpoint
-    ckpt_denoiser_step: int = 400000
-    ckpt_initp_step: int = 400000
-    #ckpt_denoiser: str = f'/home/mr6744/ckpts/08032023_032544/ckpt_denoiser_{ckpt_denoiser_step}.pt'
-    ckpt_denoiser: str = f'/scratch/mr6744/ckpts/08032023_032544/ckpt_denoiser_{ckpt_denoiser_step}.pt'
-    #ckpt_initp: str = f'/home/mr6744/ckpts/08032023_032544/ckpt_initp_{ckpt_initp_step}.pt'
-    ckpt_initp: str = f'/scratch/mr6744/ckpts/08032023_032544/ckpt_initp_{ckpt_initp_step}.pt'
+    def __init__(self, argv):
+        # Number of channels in the image. 3 for RGB.
+        self.image_channels: int = 3
+        # Image size
+        self.image_size: int = 128
+        # Number of channels in the initial feature map
+        self.n_channels: int = 32
+        # The list of channel numbers at each resolution.
+        # The number of channels is `channel_multipliers[i] * n_channels`
+        self.channel_multipliers: List[int] = [1, 2, 3, 4]
+        # The list of booleans that indicate whether to use attention at each resolution
+        self.is_attention: List[int] = [False, False, False, False]
+        # Number of time steps $T$
+        self.n_steps: int = 1_000
+        # noise scheduler Beta_0
+        self.beta_0 = 1e-6 # 0.000001
+        # noise scheduler Beta_T
+        self.beta_T = 1e-2 # 0.01
+        # Batch size
+        self.batch_size: int = argv.batch_size
+        # L2 loss
+        self.alpha = argv.l2_loss
+        # Threshold Regularizer
+        self.threshold = argv.threshold
+        # Learning rate
+        self.learning_rate: float = argv.d_lr
+        self.learning_rate_init: float = argv.g_lr
+        # Weight decay rate
+        self.weight_decay_rate: float = 1e-3
+        # ema decay
+        self.betas = (0.9, 0.999)
+        # Number of training epochs
+        self.epochs: int = 1_000_000
+        # Number of samples (evaluation)
+        self.n_samples: int = argv.sample_size
+        # Use wandb
+        self.wandb: bool = argv.wandb
+        #self.store_checkpoints: str = '/home/mr6744/ckpts/'
+        self.store_checkpoints: str = '/scratch/mr6744/pytorch/ckpts/'
+        #self.dataset_t: str = f'/home/mr6744/{argv.dataset_t}/'
+        self.dataset_t: str = f'/scratch/mr6744/pytorch/{argv.dataset_t}/'
+        #self.dataset_v: str = f'/home/mr6744/{argv.dataset_v}/'
+        self.dataset_v: str = f'/scratch/mr6744/pytorch/{argv.dataset_v}/'
+        # load from a checkpoint
+        self.ckpt_step: int = argv.ckpt_step
+        #self.ckpt_denoiser: str = f'/home/m6744/ckpts/{argv.ckpt_path}/ckpt_denoiser_{self.ckpt_step}.pt'
+        self.ckpt_denoiser: str = f'/scratch/mr6744/pytorch/ckpts/{argv.ckpt_path}/ckpt_denoiser_{self.ckpt_step}.pt'
+        #self.ckpt_initp: str = f'/home/mr6744/ckpts/{argv.ckpt_path}/ckpt_initp_{self.ckpt_step}.pt'
+        self.ckpt_initp: str = f'/scratch/mr6744/pytorch/ckpts/{argv.ckpt_path}/ckpt_initp_{self.ckpt_step}.pt'
+        self.multiplier = argv.multiplier
+        self.num_workers = argv.num_workers
+        self.sampling_interval = argv.sampling_interval
+        self.seed = argv.random_seed
 
     def init(self, rank: int, world_size: int):
         # gpu id
@@ -162,11 +165,9 @@ class Trainer():
         self.initp = DDP(self.initp, device_ids=[self.gpu_id])
 
         # only loads checkpoint if model is trained
-        if self.ckpt_denoiser_step != 0:
+        if self.ckpt_step != 0:
             checkpoint_d = torch.load(self.ckpt_denoiser)
             self.denoiser.module.load_state_dict(checkpoint_d)
-        
-        if self.ckpt_initp_step != 0:
             checkpoint_i = torch.load(self.ckpt_initp)
             self.initp.module.load_state_dict(checkpoint_i)
 
@@ -181,11 +182,11 @@ class Trainer():
         )
 
         # Create dataloader (shuffle False for validation)
-        dataset_train = Data(path=self.dataset_t, mode="train", size=(self.image_size,self.image_size), multiplier=3_200)
+        dataset_train = Data(path=self.dataset_t, mode="train", size=(self.image_size,self.image_size), multiplier=self.multiplier)
 
         self.dataloader_train = DataLoader(dataset=dataset_train,
                                             batch_size=self.batch_size // self.world_size, 
-                                            num_workers=8, #os.cpu_count() // 2,
+                                            num_workers=self.num_workers, #os.cpu_count() // 2,
                                             drop_last=False,
                                             shuffle=False, 
                                             pin_memory=False,
@@ -214,7 +215,7 @@ class Trainer():
 
         with torch.no_grad():
 
-            torch.manual_seed(7)
+            torch.manual_seed(self.seed)
             sharp, blur = next(iter(dataloader))
             
             sharp = sharp.to(self.gpu_id)
@@ -412,7 +413,7 @@ class Trainer():
                 #title = f"Init - D:{self.num_params_denoiser//1_000_000}M, G:{self.num_params_init//1_000_000}M, Pre:No, D:{'{:.0e}'.format(self.learning_rate)}, G:{'{:.0e}'.format(self.learning_rate_init)}, B:{self.batch_size}"
                 #plot_channels(steps, R, G, B, self.exp_path, title=title, ext="init_")
 
-            if ((self.step % 10_000) == 0) and (self.gpu_id == 0):
+            if ((self.step % self.sampling_interval) == 0) and (self.gpu_id == 0):
                 self.sample("train2", self.dataset_v, psnr_init_t, ssim_init_t, psnr_deblur_t, ssim_deblur_t)
                 self.sample("val", self.dataset_v, psnr_init_v, ssim_init_v, psnr_deblur_v, ssim_deblur_v)
                 sample_steps.append(self.step + self.ckpt_denoiser_step)
@@ -422,7 +423,7 @@ class Trainer():
                 torch.save(self.denoiser.module.state_dict(), os.path.join(self.exp_path, f'ckpt_denoiser_{self.ckpt_denoiser_step+self.step}.pt'))
                 torch.save(self.initp.module.state_dict(), os.path.join(self.exp_path, f'ckpt_initp_{self.ckpt_initp_step+self.step}.pt'))
 
-def ddp_setup(rank, world_size):
+def ddp_setup(rank, world_size, port):
     """
     Args:
         rank: unique identifier of each process
@@ -431,13 +432,13 @@ def ddp_setup(rank, world_size):
     # IP address of machine running rank 0 process
     # master: machine coordinates communication across processes
     os.environ["MASTER_ADDR"] = "localhost" # we assume a single machine setup)
-    os.environ["MASTER_PORT"] = "12359" # any free port on machine
+    os.environ["MASTER_PORT"] = "123" + port # any free port on machine
     # nvidia collective comms library (comms across CUDA GPUs)
     init_process_group(backend="nccl", rank=rank, world_size=world_size)
 
-def main(rank: int, world_size:int):
-    ddp_setup(rank=rank, world_size=world_size)
-    trainer = Trainer()
+def main(rank: int, world_size:int, argv):
+    ddp_setup(rank=rank, world_size=world_size, port=argv.port)
+    trainer = Trainer(argv)
     trainer.init(rank, world_size) # initialize trainer class
 
     #### Track Hyperparameters with WANDB####
@@ -469,6 +470,44 @@ def main(rank: int, world_size:int):
     destroy_process_group()
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=str, default='50')
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--sample_size', type=int, default=32)
+    parser.add_argument('--d_lr', type=float, default=1e-4)
+    parser.add_argument('--g_lr', type=float, default=1e-4)
+    parser.add_argument('--threshold', type=float, default=0.02)
+    parser.add_argument('--l2_loss', type=float, default=0.)
+    parser.add_argument('--dataset_t', type=str, default="gopro")
+    parser.add_argument('--dataset_v', type=str, default="gopro_128")
+    parser.add_argument('--ckpt_step', type=int, default=0)
+    parser.add_argument('--ckpt_path', type=str, default="")
+    parser.add_argument('--num_workers', type=int, default=8)
+    parser.add_argument('--multiplier', type=int, default=1)
+    parser.add_argument('--sampling_interval', type=int, default=10_000)
+    parser.add_argument('--random_seed', type=int, default=7)
+    parser.add_argument('--name', type=str, default="conditioned")
+    parser.add_argument('--wandb', action="store_true")
+    argv = parser.parse_args()
+
+    print('port:', argv.port, type(argv.port))
+    print('batch_size:', argv.batch_size, type(argv.batch_size))
+    print('sample_size:', argv.sample_size, type(argv.sample_size))
+    print('d_lr:', argv.d_lr, type(argv.d_lr))
+    print('g_lr:', argv.g_lr, type(argv.g_lr))
+    print('threshold:', argv.threshold, type(argv.threshold))
+    print('l2_loss:', argv.l2_loss, type(argv.l2_loss))
+    print('dataset_t:', argv.dataset_t, type(argv.dataset_t))
+    print('dataset_v:', argv.dataset_v, type(argv.dataset_v))
+    print('ckpt_step:', argv.ckpt_step, type(argv.ckpt_step))
+    print('ckpt_path:', argv.ckpt_path, type(argv.ckpt_path))
+    print('num_workers:', argv.num_workers, type(argv.num_workers))
+    print('multiplier:', argv.multiplier, type(argv.multiplier))
+    print('sampling_interval:', argv.sampling_interval, type(argv.sampling_interval))
+    print('random_seed:', argv.random_seed, type(argv.random_seed))
+    print('name:', argv.name, type(argv.name))
+    print('wandb:', argv.wandb, type(argv.wandb))
+
     world_size = torch.cuda.device_count() # how many GPUs available in the machine
-    #world_size = 2
-    mp.spawn(main, args=(world_size,), nprocs=world_size)
+    mp.spawn(main, args=(world_size,argv), nprocs=world_size)
